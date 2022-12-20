5474e1e84e5a63f11a299d79b14e48d979d15b25
ellyhp <ellygmr_13@hotmail.com>
2022-12-20 09:22:01 -0600

    v5

---
+++ main.js
@@ -0,0 +1,92 @@
+const iconCarrito = document.querySelector('.nav--carrito');
+const shop = document.querySelector('#shop');
+const ShoppingContainer = document.querySelector('.shopping--container');
+const CarContainer = document.querySelector('#carList tbody');
+const DeleteCar = document.querySelector('.btn-emptycart')
+let myCarShop = [];
+
+
+
+EventListenerRegister();
+function EventListenerRegister(){
+
+    iconCarrito.addEventListener('click', ShoppingCar);
+    shop.addEventListener('click', shopProduct);
+ 
+}
+
+
+function ShoppingCar(){
+    ShoppingContainer.classList.toggle('inactive');
+}
+
+function shopProduct(e){
+
+    if(e.target.classList.contains('boton--tarjeta')){
+        const selectCourse = e.target.parentElement;
+        DataProductRead(selectCourse);
+    }
+}
+
+function DataProductRead(product){
+    const ProductInfo = {
+        image: product.querySelector('img').src,
+        tittle: product.querySelector('p').textContent,
+        precio: product.querySelector('h3').textContent,
+        id: product.querySelector('button').getAttribute('data-id'),
+        cantidad: 1
+    }
+    
+    myCarShop = [...myCarShop, ProductInfo];
+    console.log(myCarShop);
+
+    HTMLCar();
+}
+
+function HTMLCar(){
+
+
+    HTMLDelete();
+
+    myCarShop.forEach(product=> {
+        const { image, tittle,precio,cantidad,id} = product;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${image}" width="60px">
+            </td>
+            <td>
+                ${tittle}
+            </td>
+            <td>
+                ${precio}
+            </td>
+            <td>
+                ${cantidad}
+            </td>
+            <td>
+                    <i class="fa-solid fa-xmark  icon--delete" data-id="${id}"></i>
+            </td>
+        `;
+
+        CarContainer.appendChild(row);
+
+    });
+}
+
+
+function HTMLDelete(){
+    // CarContainer.innerHTML = '';
+
+    while(CarContainer.firstChild){
+        CarContainer.removeChild(CarContainer.firstChild)
+    }
+}
+
+
+DeleteCar.addEventListener('click', DeleteAllCar);
+
+function DeleteAllCar(){
+    CarContainer.remove(CarContainer)
+}
+
